880a802e06f32a1a148c117d4f9598da42c06610
Bhushan Thapa <95372432+bhushanthapa@users.noreply.github.com>
2022-07-16 13:14:33 +0530

    Add initial files

---
+++ script.js
@@ -0,0 +1,14 @@
+function myfun(){}
+   var mysong = document.getElementById("mysong");
+    var icon = document.getElementById("icon");
+
+    icon.onclick = function(){
+        if(mysong.paused){
+            mysong.play();
+            icon.src = "pause.png";
+        }else{
+            mysong.pause();
+            icon.src = "play.png";
+        }
+    }
+}
